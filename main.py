538ea721a593bad7a825033351b80ad40db1cd6e
Shristi Sehwag <91873414+ShristiSehwag@users.noreply.github.com>
2024-07-12 10:21:59 +0000

    extract data via url

--- main.py
+++ main.py
@@ -1,55 +1,28 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import requests
+from datetime import datetime
 import time
 
-# login , click, scrape
+ticker = input("Enter a ticker symbol (Eg : AAPL,MSFT): ")
+from_date = input('From Date (YYYY-MM-DD): ')
+to_date = input('To Date (YYYY-MM-DD): ')
 
+from_datetime = datetime.strptime(from_date, '%Y/%m/%d')
+to_datetime = datetime.strptime(to_date, '%Y/%m/%d')
 
-def get_driver():
+from_epoch = int(time.mktime(from_datetime.timetuple()))
+to_epoch = int(time.mktime(to_datetime.timetuple()))
 
-  #set options for easier browsing
-  options = webdriver.ChromeOptions()
-  options.add_argument('disaable-infobars')
-  options.add_argument('start-maximized ')
-  options.add_argument('disable-dev-shm-usage')
-  options.add_argument('no-sandbox') #gain more privilage
-  options.add_experimental_option('excludeSwitches', ['enable-automation'])
-  options.add_argument('disable-blink-features=AutomationControlled')
+url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
 
-  driver = webdriver.Chrome(options)
-  driver.get('https://automated.pythonanywhere.com/login/')
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"
+}
 
-  return driver 
+#bite object
+content = requests.get(url, headers= headers ).content
+print(content)
 
-def clean_text(text):
-  """Extract only the temperature from text"""
-  # gives list of text, convert to float
-  output = float(text.split(": ")[1])
-  return output
-
-
-def main():
-  driver = get_driver()
-  
-  ## LOGIN - enter text
-  driver.find_element(by='id',value ='id_username').send_keys("automated")
-  
-  time.sleep(2) #delay task
-  
-  driver.find_element(by='id',value ='id_password').send_keys("automatedautomated" + Keys.RETURN) # CLICK submit (with return(acts like enter key))
-  # print(driver.current_url) #print new web page url
-  time.sleep(2)
-
-  
-  #Go to home
-  driver.find_element(by='xpath',value ='/html/body/nav/div/a').click()
-  time.sleep(2)
+with open('data3.csv', 'wb') as file:
+  file.write(content)
   
 
-  text = driver.find_element(by='xpath',value ='/html/body/div[1]/div/h1[2]')
-  return clean_text(text.text)
-
-  
-  
-print(main())
 
