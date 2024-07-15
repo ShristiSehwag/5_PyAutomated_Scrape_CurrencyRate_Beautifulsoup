c554736770c4497c422df3ca03b347fb303fb74e
Shristi Sehwag <91873414+ShristiSehwag@users.noreply.github.com>
2024-07-15 06:35:51 +0000

    Extract data using beautifulsoup

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
+# Beautiful soup
+# scrape real time curency rates
+
+from bs4 import BeautifulSoup
 import requests
-from datetime import datetime
-import time
 
-ticker = input("Enter a ticker symbol (Eg : AAPL,MSFT): ")
-from_date = input('From Date (YYYY-MM-DD): ')
-to_date = input('To Date (YYYY-MM-DD): ')
+def get_currency(in_currency, out_currency):
+  url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
+  content = requests.get(url).text
+  soup = BeautifulSoup(content, 'html.parser')
+  rate = soup.find("span", class_="ccOutputRslt").get_text()
+  rate = float(rate[:-4])
 
-from_datetime = datetime.strptime(from_date, '%Y/%m/%d')
-to_datetime = datetime.strptime(to_date, '%Y/%m/%d')
+  return rate
+  
+  # print(content) 
+  # returns the entire source code of the website
+current_rate = get_currency('USD', 'INR')
+print(current_rate)
 
-from_epoch = int(time.mktime(from_datetime.timetuple()))
-to_epoch = int(time.mktime(to_datetime.timetuple()))
-
-url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
-
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"
-}
-
-#bite object
-content = requests.get(url, headers= headers ).content
-print(content)
-
-with open('data3.csv', 'wb') as file:
-  file.write(content)
   
 
 
